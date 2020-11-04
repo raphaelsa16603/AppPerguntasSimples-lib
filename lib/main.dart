c4aa1f80f62eb7946888dd7ab35a36e609c31f4e
raphaelsa16603 <raphaelsa16603@gmail.com>
2020-11-04 20:32:10 -0300

    debugShowCheckedModeBanner: false,

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
